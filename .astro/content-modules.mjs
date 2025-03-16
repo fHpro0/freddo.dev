698514433463b30137541966f5be9687738b9035
fHpro0 <fHpro0@users.noreply.github.com>
2025-03-16 10:42:26 +0100

    removed pnpm to bun; updated packages; updated link and updated skills

---
+++ .astro/content-modules.mjs
@@ -0,0 +1,4 @@
+
+export default new Map([
+["src/content/post/hello-world.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpost%2Fhello-world.mdx&astroContentModuleFlag=true")]]);
+		
