9f381798d78f529829fc7414a7ce7bc4a1cea1de
fHpro0 <fHpro0@users.noreply.github.com>
2026-06-16 22:53:48 +0200

    update

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,4 +1,7 @@
 
 export default new Map([
-["src/content/post/hello-world.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpost%2Fhello-world.mdx&astroContentModuleFlag=true")]]);
+["src/content/post/hello-world.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpost%2Fhello-world.mdx&astroContentModuleFlag=true")],
+["src/content/post/ai-dev-tools.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpost%2Fai-dev-tools.mdx&astroContentModuleFlag=true")],
+["src/content/post/typescript-tips.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpost%2Ftypescript-tips.mdx&astroContentModuleFlag=true")],
+["src/content/post/building-with-astro.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpost%2Fbuilding-with-astro.mdx&astroContentModuleFlag=true")]]);
 		
